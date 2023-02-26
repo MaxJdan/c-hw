07a71e3ca0985ddf51bb6d2f7442cd2052bdc6a7
Max Jdanov <maxzhdanov@list.ru>
2023-02-26 20:19:11 +0300

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akker(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akker(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akker(m - 1, 1);
+    else
+        return Akker(m - 1, Akker(m, n - 1));
+}
