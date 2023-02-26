d74646e9f313d57946fd7d9b1d50bb481ea706b0
Max Jdanov <maxzhdanov@list.ru>
2023-02-26 20:18:35 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountSum(m, n - 1);
+}
